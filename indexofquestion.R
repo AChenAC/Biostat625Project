81b5597486bacba02a63e4138e27f6c8144ea351
miraclethief <72032088+miraclethief@users.noreply.github.com>
2021-12-11 14:17:56 -0500

    Add files via upload

---
+++ indexofquestion.R
@@ -0,0 +1,46 @@
+setwd("D:/625 final project")
+data <- load('cleaned_data.RData')
+data <- data.frame(cleaned_data)
+
+
+
+
+resultpublic <- read.csv("survey_results_public.csv")
+resultname <- colnames(resultpublic)
+name <- colnames(data)
+name <- name[1:357]
+name[145:146] <- c("JobSatisfaction", "CareerSatisfaction")
+
+question <- c()
+for (i in 1:357) {
+  if(grepl("_", name[i], fixed = TRUE)){
+     question <- cbind(question, strsplit(name[i], "[_]")[[1]][1])
+  }else{
+     question <- cbind(question, strsplit(name[i], "[..]")[[1]][1])
+  }
+}
+
+
+indexofquestion <- c()
+for (i in 1:357) {
+  for (j in 1:129) {
+    if(question[i] == resultname[j]){
+      indexofquestion <- cbind(indexofquestion, rbind(j,name[i]))
+      break    
+    }
+    # if(j==129){
+    #   cat(i,"\n")
+    # }
+  }
+}
+
+
+continent <- cbind(rbind("Not in result","Continent"),rbind("Not in result","Continent"),rbind("Not in result","Continent"),rbind("Not in result","Continent"))
+
+ncol(indexofquestion)
+indexofquestion <- cbind(indexofquestion[,1:84], continent,indexofquestion[,85:351])
+indexofquestion <- cbind(indexofquestion,rbind("Not in result",name[356]))
+indexofquestion <- cbind(indexofquestion,rbind("Not in result",name[357]))
+indexofquestion[2,145] <- "V28"
+indexofquestion[2,146] <- "V29"
+
